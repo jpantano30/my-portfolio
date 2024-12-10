fa2ae0fd3625ef426646a388030e96b2c19ed3e7
Jena <jpantano30@gmail.com>
2024-12-10 17:05:13 -0500

    updated paths

--- src/components/GetToppingImage.jsx
+++ src/components/GetToppingImage.jsx
@@ -1,13 +1,13 @@
 async function getToppingImage(toppingId) {
   switch (toppingId) {
     case 'strawberry':
-      return (await import('/Users/jenapantano/BU Masters/cs601fall24/Project/my-portfolio/src/assets/strawberry.png')).default
+      return (await import('../assets/strawberry.png')).default
     case 'cherry':
-      return (await import('/Users/jenapantano/BU Masters/cs601fall24/Project/my-portfolio/src/assets/cherry.png')).default
+      return (await import('../assets/cherry.png')).default
     case 'sprinkles':
-      return (await import('/Users/jenapantano/BU Masters/cs601fall24/Project/my-portfolio/src/assets/sprinkles.png')).default
+      return (await import('../assets/sprinkles.png')).default
     case 'candle':
-      return (await import('/Users/jenapantano/BU Masters/cs601fall24/Project/my-portfolio/src/assets/candle.png')).default
+      return (await import('../assets/candle.png')).default
     default:
       throw new Error('Unknown topping')
   }
